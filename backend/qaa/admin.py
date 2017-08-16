feb5b1268a08506b7e39a4fc66d1b0d1f3a7eeb4
unknown <zhangkang103@gmail.com>
2017-08-17 00:46:02 +0800

    [功能] 添加后台管理页面

--- backend/qaa/admin.py
+++ backend/qaa/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+  list_display = ('q_id', 'short', 'content')
+
+admin.site.register(Question, QuestionAdmin)
