0f2bf7087291189726005ea38bc7405fb3ca5422
unknown <zhangkang103@gmail.com>
2017-08-18 23:42:25 +0800

    [功能] 注册 qaa 路径

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'backend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^qaa/', include('qaa.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
