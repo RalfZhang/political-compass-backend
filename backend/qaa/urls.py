c4d4521bb857874c21fb1f40c5010638d000151f
unknown <zhangkang103@gmail.com>
2017-08-19 00:57:09 +0800

    [功能] 添加所有问题与单个问题字符串输出接口

--- backend/qaa/urls.py
+++ backend/qaa/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.index),
-  # url(r'^questions$', views.questions,)
+  url(r'^$', views.index, name='index'),
+  url(r'^questions$', views.questions),
+  url(r'^questions/(?P<question_id>[0-9]+)/$', views.question),
 ]
--- backend/qaa/views.py
+++ backend/qaa/views.py
@@ -3,9 +3,15 @@ from django.http import HttpResponse
 from .models import Question
 
 # Create your views here.
+
 def index(request):
-  questions = Question.objects
-  output = ', '.join(p.short for p in questions)
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def questions(request):
+  questions = Question.objects.all()
+  output = ', '.join(p.__str__() for p in questions)
   return HttpResponse(output)
-# def questions(request):
-#   return 'hello'
+def question(request, question_id):
+  id = int(question_id)
+  question = Question.objects.get(id=id)
+  return HttpResponse(question.__str__())
