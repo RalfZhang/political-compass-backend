8ac5309be8d3618882a323ad86d479230e18dbce
unknown <zhangkang103@gmail.com>
2017-08-18 23:59:01 +0800

    [更新] 更新 qaa 默认请求

--- backend/qaa/views.py
+++ backend/qaa/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
+def index(request):
+  questions = Question.objects
+  output = ', '.join(p.short for p in questions)
+  return HttpResponse(output)
+# def questions(request):
+#   return 'hello'
