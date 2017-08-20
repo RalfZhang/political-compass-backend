d3b18e47cca6332ac65317120473b431f065d7db
unknown <zhangkang103@gmail.com>
2017-08-20 17:31:17 +0800

    [功能] 注册 rest framework url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+import sys
+sys.path.append("..")
+from qaa import views
+
+router = routers.DefaultRouter()
+router.register(r'questions', views.QuestionViewSet)
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'backend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^qaa/', include('qaa.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
