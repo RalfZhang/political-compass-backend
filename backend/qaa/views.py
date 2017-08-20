42890616653d5da2be8b80031a5c6ad847849e95
unknown <zhangkang103@gmail.com>
2017-08-20 17:29:52 +0800

    [功能] 添加 rest framework model

--- backend/qaa/views.py
+++ backend/qaa/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+
 from .models import Question
+from .serializers import QuestionSerializer
 
 # Create your views here.
+class QuestionViewSet(viewsets.ModelViewSet):
+  queryset = Question.objects.all().order_by('order_id')
+  serializer_class = QuestionSerializer
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
