1fdb11006df510489588138d304d3df7b32ce55c
unknown <zhangkang103@gmail.com>
2017-09-07 23:08:30 +0800

    [更新] 注册 choice admin

--- backend/qaa/admin.py
+++ backend/qaa/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Answer
+from .models import Question, Answer, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
   list_display = ('q_id', 'short', 'content')
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
+admin.site.register(Choice)
