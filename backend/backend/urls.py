f75ce6b3bd45295b702023a4be370eff678becd9
unknown <zhangkang103@gmail.com>
2017-09-10 23:53:50 +0800

    [更新] 取消 qaa 的 url 入口

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^api/question/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^qaa/', include('qaa.urls')),
+    # url(r'^qaa/', include('qaa.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
