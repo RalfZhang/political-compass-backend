3ed7a2767c0f217de6124638910195b7ba9e78bd
unknown <zhangkang103@gmail.com>
2017-09-25 23:31:55 +0800

    [更新] choice admin 列表

--- backend/qaa/admin.py
+++ backend/qaa/admin.py
@@ -6,6 +6,9 @@ from .models import Question, Answer, Choice
 class QuestionAdmin(admin.ModelAdmin):
   list_display = ('q_id', 'short', 'content')
 
+class ChoiceAdmin(admin.ModelAdmin):
+  list_display = ('id', 'content', 'order_id' ,'group_id',)
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
