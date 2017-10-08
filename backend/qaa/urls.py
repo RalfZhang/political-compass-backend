027fd742fffd88bf7ad9e5e7f3ddbfcd27d3f2ed
unknown <zhangkang103@gmail.com>
2017-10-08 23:32:49 +0800

    [更新] 语义化接口方法

--- backend/qaa/urls.py
+++ backend/qaa/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^questions$', views.questions),
-  url(r'^questions/(?P<question_id>[0-9]+)$', views.question),
+  url(r'^questions$', views.getQuestionList),
+  url(r'^questions/(?P<question_id>[0-9]+)$', views.getQuestionById),
 ]
