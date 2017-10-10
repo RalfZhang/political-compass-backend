4ba0e768fffba7bfeaf6f4ad7d42ce3dabd82e07
unknown <zhangkang103@gmail.com>
2017-10-10 23:35:31 +0800

    [修复] 去除不必要的识别

--- backend/qaa/views.py
+++ backend/qaa/views.py
@@ -49,10 +49,6 @@ def getQuestionById(request, question_id):
 
 # 提交答案
 def postAnswer(request):
-  # 非 post 即返回
-  if request.method != 'POST':
-    return Http404('')
-  a102 = request.POST['q102'] # todo
   try:
     answer = Answer(time = timezone.now(), q101 = int(request.POST['q101']), q102 = int(request.POST['q102']), q104 = int(request.POST['q104']), q103 = int(request.POST['q103']), q106 = int(request.POST['q106']), q105 = int(request.POST['q105']), q107 = int(request.POST['q107']), q108 = int(request.POST['q108']), q109 = int(request.POST['q109']), q111 = int(request.POST['q111']), q110 = int(request.POST['q110']), q112 = int(request.POST['q112']), q113 = int(request.POST['q113']), q114 = int(request.POST['q114']), q115 = int(request.POST['q115']), q117 = int(request.POST['q117']), q119 = int(request.POST['q119']), q116 = int(request.POST['q116']), q120 = int(request.POST['q120']), q118 = int(request.POST['q118']), q301 = int(request.POST['q301']), q302 = int(request.POST['q302']), q303 = int(request.POST['q303']), q304 = int(request.POST['q304']), q305 = int(request.POST['q305']), q306 = int(request.POST['q306']), q309 = int(request.POST['q309']), q307 = int(request.POST['q307']), q308 = int(request.POST['q308']), q310 = int(request.POST['q310']), q311 = int(request.POST['q311']), q312 = int(request.POST['q312']), q313 = int(request.POST['q313']), q314 = int(request.POST['q314']), q316 = int(request.POST['q316']), q317 = int(request.POST['q317']), q318 = int(request.POST['q318']), q319 = int(request.POST['q319']), q320 = int(request.POST['q320']), q315 = int(request.POST['q315']), q201 = int(request.POST['q201']), q202 = int(request.POST['q202']), q203 = int(request.POST['q203']), q204 = int(request.POST['q204']), q205 = int(request.POST['q205']), q206 = int(request.POST['q206']), q207 = int(request.POST['q207']), q208 = int(request.POST['q208']), q209 = int(request.POST['q209']), q210 = int(request.POST['q210']), q1001 = int(request.POST['q1001']), q1002 = int(request.POST['q1002']), q1003 = int(request.POST['q1003']), q1004 = int(request.POST['q1004']), ip = '123', device = 'ddddmode')
     answer.save()
